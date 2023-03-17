5396281a7b720614288991f0eced8ce3d986b62d
leese00 <122573963+leese00@users.noreply.github.com>
2023-03-17 17:18:25 -0400

    in process

---
+++ 03-Juicy-Brick-Breaker-1-main/Wall/Wall.gd
@@ -0,0 +1,19 @@
+extends StaticBody2D
+
+var decay = 0.01
+
+func _ready():
+	pass
+
+func _physics_process(_delta):
+	if $ColorRect.color.s > 0:
+		$ColorRect.color.s -= decay
+	if $ColorRect.color.v > 0:
+		$ColorRect.color.v += decay
+
+func hit():
+	$ColorRect.color = Color8(51,154,240)
+	
+	var wall_sound = get_node_or_null("/root/Game/Wall_Sound")
+	if wall_sound != null:
+		wall_sound.play()
